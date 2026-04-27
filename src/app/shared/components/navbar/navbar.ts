74728e80b40c689193db841f29ef571d550a2042
Miguel Ordonez Camara <miguelordonezcamara@hotmail.com>
2026-04-27 11:30:43 -0600

    build

---
+++ src/app/shared/components/navbar/navbar.ts
@@ -0,0 +1,45 @@
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { routes } from '../../../app.routes';
+import { filter, map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [AsyncPipe, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styles: ``,
+})
+export class Navbar {
+  router = inject(Router);
+
+  routes = routes
+    .map((route) => ({
+      path: route.path,
+      title: `${route.title ?? 'Maps en Angular'}`,
+    }))
+    .filter((route) => route.path !== '**');
+
+  pageTitle$ = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    // tap((event) => console.log(event)),
+    map((event) => event.url),
+    map(
+      (url) =>
+        routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas'
+    )
+  );
+
+  pageTitle = toSignal(
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      // tap((event) => console.log(event)),
+      map((event) => event.url),
+      map(
+        (url) =>
+          routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas'
+      )
+    )
+  );
+}
